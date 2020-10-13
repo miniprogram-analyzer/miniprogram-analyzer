2655cf07a04718e244097718e100e350433f3e73
LanceZhu <lancezhu2937@gmail.com>
2020-10-13 22:33:36 +0800

    docs: update README

--- report/weiyou/plato/files/example_weiyou_app_js/report.history.js
+++ report/weiyou/plato/files/example_weiyou_app_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 13 Oct 2020 08:39:24 GMT","sloc":12,"lloc":5,"functions":3,"deliveredBugs":0.021,"difficulty":2.571,"maintainability":100,"lintErrors":1}]
+__history = [{"date":"Tue, 13 Oct 2020 14:31:15 GMT","sloc":12,"lloc":5,"functions":3,"deliveredBugs":0.021,"difficulty":2.571,"maintainability":100,"lintErrors":1}]
